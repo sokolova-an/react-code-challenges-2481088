a49c1d6d5b93ba0168e18a2a6ca312d921d2c531
Anastasiia Sokolova <dev.anastasis@gmail.com>
2023-05-28 15:58:11 +0700

    p6

--- src/06-add-window-event/ToggleWindowEvent.js
+++ src/06-add-window-event/ToggleWindowEvent.js
@@ -1,12 +1,15 @@
-import { useState } from 'react'
-import WindowEvent from './WindowEvent'
+import {  useState } from "react";
+import WindowEvent from "./WindowEvent";
 
-export default function ToggleWindowEvent () {
-  const [windowEvent, setWindowEvent] = useState(false)
+export default function ToggleWindowEvent() {
+  const [windowEvent, setWindowEvent] = useState(false);
+
   return (
     <div>
-      <button onClick={() => setWindowEvent(prevState => !prevState)}>Toggle Window Event</button>
+      <button onClick={() => setWindowEvent((prevState) => !prevState)}>
+        Toggle Window Event
+      </button>
       {windowEvent && <WindowEvent />}
     </div>
-  )
+  );
 }
--- src/06-add-window-event/WindowEvent.js
+++ src/06-add-window-event/WindowEvent.js
@@ -1,5 +1,15 @@
-export default function WindowEvent () {
-  return (
-    <h2>Window event active</h2>
-  )
+import { useEffect } from "react";
+
+export default function WindowEvent() {
+  useEffect(() => {
+    const dblclick = () => {
+      alert("Double-clicked!");
+    };
+    window.addEventListener("dblclick", dblclick);
+    return () => {
+      window.removeEventListener("dblclick", dblclick);
+    };
+  }, []);
+
+  return <h2>Window event active</h2>;
 }
