08613713bdc45afad5e5e5ade2da80b240df9231
Anastasiia Sokolova <dev.anastasis@gmail.com>
2023-05-29 15:33:59 +0700

    p8

--- src/08-pixel-art/PixelArt.js
+++ src/08-pixel-art/PixelArt.js
@@ -1,34 +1,60 @@
-import React from 'react'
+import React, { useState } from "react";
 
-function ColorPicker () {
-  const colors = ['red', 'blue', 'yellow', 'green', 'black', 'white', 'purple']
+function ColorPicker({ setSelectedColor }) {
+  const colors = ["red", "blue", "yellow", "green", "black", "white", "purple"];
   return (
     <div>
       <h1>Choose a color</h1>
-      {colors.map(color => <button key={color} style={{ backgroundColor: color }} />)}
+      {colors.map((color) => (
+        <button
+          key={color}
+          style={{ backgroundColor: color, border: "1px solid black" }}
+          onClick={() => setSelectedColor(color)}
+        />
+      ))}
     </div>
-  )
+  );
 }
 
-function Pixel () {
-  return <div style={{ height: '20px', width: '20px', backgroundColor: 'lightGrey', margin: '1px' }} />
-}
-
-function Pixels () {
-  const pixels = []
-  for (let i = 0; i < 100; i++) pixels.push(<Pixel key={i} />)
+function Pixel({ selectedColor }) {
+  const [backgroundColor, setBackgroundColor] = useState("lightGrey");
   return (
-    <div style={{ display: 'grid', gridTemplateColumns: 'repeat(10, 1fr)', width: '210px', margin: '0 auto' }}>
+    <button
+      onClick={() => setBackgroundColor(selectedColor)}
+      style={{
+        height: "20px",
+        width: "20px",
+        backgroundColor,
+        margin: "1px",
+      }}
+    />
+  );
+}
+
+function Pixels({ selectedColor }) {
+  const pixels = [];
+  for (let i = 0; i < 100; i++)
+    pixels.push(<Pixel key={i} selectedColor={selectedColor} />);
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(10, 1fr)",
+        width: "210px",
+        margin: "0 auto",
+      }}
+    >
       {pixels}
     </div>
-  )
+  );
 }
 
-export default function PixelArt () {
+export default function PixelArt() {
+  const [selectedColor, setSelectedColor] = React.useState("");
   return (
     <div>
-      <ColorPicker />
-      <Pixels />
+      <ColorPicker setSelectedColor={setSelectedColor} />
+      <Pixels selectedColor={selectedColor} />
     </div>
-  )
+  );
 }
